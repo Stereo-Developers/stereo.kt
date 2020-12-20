ea7baa40101b1019635bd4d280c93aa70f13c565
Sxmurai <gavrw0607@gmail.com>
2020-12-20 15:33:03 -0600

    status shit

--- src/main/kotlin/xyz/stereobot/kt/Main.kt
+++ src/main/kotlin/xyz/stereobot/kt/Main.kt
@@ -2,6 +2,8 @@ package xyz.stereobot.kt
 
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter
 import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.OnlineStatus
+import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.requests.GatewayIntent
 import net.dv8tion.jda.api.utils.MemberCachePolicy
 import net.dv8tion.jda.api.utils.cache.CacheFlag
@@ -14,6 +16,8 @@ fun main(args: Array<String>) {
   
   JDABuilder.createLight(config.get<String>("bot.token"))
     .addEventListeners(ListenerHandler(waiter), waiter)
+    .setActivity(Activity.listening("${config.get<List<String>>("bot.prefixes")[0]}help"))
+    .setStatus(OnlineStatus.DO_NOT_DISTURB)
     .setMemberCachePolicy(MemberCachePolicy.VOICE)
     .enableIntents(GatewayIntent.getIntents(641))
     .disableIntents(GatewayIntent.getIntents(32126))
